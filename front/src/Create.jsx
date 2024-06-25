673b0059d786931e975e365f661d6ab7f1c5c817
Jay Zenith <jayzenith248@gmail.com>
2024-06-25 13:46:00 -0700

    Added Home and Create componentsa and imported into App

---
+++ front/src/Create.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Create() {
+  return (
+    <div className="create_field">
+        <input type="text" name="" id="" />
+        <button type="button">Enter</button>
+    </div>
+  )
+}
+
+export default Create
